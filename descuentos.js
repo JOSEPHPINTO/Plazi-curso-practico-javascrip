a9b2f270e937b597a55a18824b7e3fbc868fbacc
Jose Pinto <alipinko123@gmail.com>
2022-03-03 17:19:07 -0500

    Se termina el taller de Descuentos y se realizan los retos todo desde Mobile First

--- descuentos.js
+++ descuentos.js
@@ -30,7 +30,106 @@ function onClickButtonPriceDiscount(){
 }
 
 const coupons = [
-    "JuanDC_es_Batman",
-    "pero_no_le_digas_a_nadie",
-    "es_un_secreto",
+    "uno",
+    "dos",
+    "tres",
 ];
+
+// forma 1 de solucionar el reto 
+
+function onClickButtonPriceDiscountCoupon1(){
+    const inputPrice = document.getElementById("InputPrice1");
+    const priceValue = inputPrice.value;
+
+    const inpuCoupon = document.getElementById("InputCoupon1");
+    const couponValue = inpuCoupon.value;
+    
+    
+    let descuento;
+        switch(couponValue){ 
+            case coupons[0]:
+                descuento = 15;
+            break;
+
+            case coupons[1]:
+                descuento = 30 ;
+             break;
+            
+            case coupons[2]:
+                descuento = 50;
+            break;
+        }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento)
+
+    const resultCoupon = document.getElementById("ResultCoupon1")
+    resultCoupon.innerText = "El precio con descuento del cupon es: $ " + precioConDescuento;
+}
+
+// Forma 2 de solucionar el reto 
+
+function onClickButtonPriceDiscountCoupon2(){
+    const inputPrice = document.getElementById("InputPrice2");
+    const priceValue = inputPrice.value;
+
+    const inpuCoupon = document.getElementById("InputCoupon2");
+    const couponValue = inpuCoupon.value;
+   
+   let descuento;
+if (!coupons.includes(couponValue)){
+        descuento = 0;
+        alert("El cupon" + couponValue + " no es válido. "
+        ) 
+    } else if (couponValue === "uno"){
+        descuento = 15;
+    }else if (couponValue === "dos"){
+        descuento = 25;
+    }else if (couponValue === "tres"){
+        descuento = 50;
+    }
+    
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento)
+
+    const resultCoupon2 = document.getElementById("ResultCoupon2")
+    resultCoupon2.innerText = "El precio con descuento del cupon es: $ " + precioConDescuento;
+}
+
+// forma 3 de solucionar el reto
+
+function onClickButtonPriceDiscountCoupon3(){
+    const coupons = [
+        {
+            name: "uno",
+            discount: 15,
+        },
+        {
+            name: "dos",
+            discount: 25,
+        },
+        {
+            name: "tres",
+            discount: 50,
+        },
+    ];
+    const inputPrice = document.getElementById("InputPrice3");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon3")
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon){
+        return coupon.name === couponValue;
+    } 
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    if (!userCoupon){
+        alert("El cupon  " + couponValue + "  no es valido");
+        
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+        const resultCoupon = document.getElementById("ResultCoupon3");
+        resultCoupon.innerText = "El precio con descuento es: $ " + precioConDescuento;
+    }
+
+}
